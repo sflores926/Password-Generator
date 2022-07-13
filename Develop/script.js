aac3018ceecaf08365cd48d768e90a923e25e3f4
Stephany M <sflores926@yahoo.com>
2022-07-13 17:56:22 -0400

    updated js

--- Develop/script.js
+++ Develop/script.js
@@ -1,39 +1,57 @@
 // Assignment Code
-var generateBtn = document.querySelector("#generate");
+var passNumberLength = 8;
 var userChoice = [];
 
-var lowercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-var uppercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 var specialChars = ['!', '@', '#', '$', '^', '%', '&', '*', '(', ')', '[', ']', '{', '}', '=', '<', '>', '/', ','];
 
 
-// console.log(specialChars);
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
-}
+var generateBtn = document.querySelector("#generate");
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
+// Write password to the #password input
+function writePassword() {
+  var correctPrompts = getPrompts();
+  var passwordText = document.querySelector("#password");
 
+  if (correctPrompts) {
+    var newPassword = generatePassword();
+    passwordText.value = newPassword;
+  } else {
+    passwordText.value = "";
+  }
+
+}
+
+
+
+ // function to generatePassword
 function generatePassword() {
-  // function generatePassword() {
+//loop for creating random password
+  var password = "";
+  for (var i = 0; i < passNumberLength; i++) {
+    var randomIndex = Math.floor(Math.random() * userChoice.length);
+    password = password + userChoice[randomIndex];
+  }
+  return password;
+
+}
+
+
+function getPrompts() {
+  userChoice = []
 // Asks user for the length of password they want to create
-  var passNumber = window.prompt("How many characters would you like your password to contain?");
-  console.log(passNumber);
-  if (passNumber < 8) {
-    window.alert("Password must be at least 8 characters.");
+  passNumberLength = parseInt(window.prompt("How many characters would you like your password to contain?"));
+  // console.log(passNumberlength);
+  if (isNaN(passNumberLength) || passNumberLength < 8 || passNumberLength > 128) {
+    alert("Password must be at between 8 - 128 numbers. Try again.");
     return;
-    } else if (passNumber > 128) {
-      window.alert("Password must be less than 129 characters.");
-      return;
-    }
+  }
+
 
 // individual prompts to confirm if lowercase, uppercase, numeric, and special characters are wanted in password
     if (window.confirm("Click to confirm to include lowercase characters.")) {
@@ -46,13 +64,7 @@ function generatePassword() {
       userChoice = userChoice.concat(specialChars);
     }
 
-//loop for creating random password
-    var password = "";
-    for (var i = 0; i < passNumber; i++) {
-      var randomIndex = Math.floor(Math.random() * userChoice.length);
-      password = password + userChoice[randomIndex];
-    }
-    return password;
-}
+    return true;
 
-// writePassword();
+  }
+   
